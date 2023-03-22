07d8742972b472b7133f80d2469cd9974c2783b3
Jonas Gonzalez <alfredojonas94@gmail.com>
2023-03-21 22:21:10 -0400

    Set initial react app version with typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Rechat <code>coding/hiring</code> assessment.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
